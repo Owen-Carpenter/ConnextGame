580a5e8fb41dee4fee25bb279100f667acf18a83
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-02-14 15:55:13 -0600

    leaderboardController implemented

--- Server/controllers/leaderboardController.js
+++ Server/controllers/leaderboardController.js
@@ -1,7 +1,32 @@
 import AuthenticationModel from "../models/authentication.js";
 
 const handleLeaderboard = {
-    
+    leaderboardRefresh: async () => {
+        try{
+            const classicLeaderboard = AuthenticationModel.find({})
+                .sort({ "games.classic.topScore": -1})
+                .limit(10)
+                .select("username games.classic.topScore");
+            
+            const infiniteLeaderboard = AuthenticationModel.find({})
+                .sort({ "games.infinite.topScore": -1})
+                .limit(10)
+                .select("username games.infinite.topScore");
+
+            const versusLeaderboard = AuthenticationModel.find({})
+                .sort({ "games.versus.topScores": -1})
+                .limit(10)
+                .select("username games.versus.topScore");
+            return{
+                classic: classicLeaderboard,
+                infinite: infiniteLeaderboard,
+                versus: versusLeaderboard
+            };
+        } catch(err){
+            console.log(err);
+            throw(err);
+        }
+    }
 }
 
 export default handleLeaderboard;
