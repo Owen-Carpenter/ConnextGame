b096b9b1499ae1a7865b99f46ad383e98a7e8cc3
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-04-16 13:08:12 -0500

    Code cleanup for production

--- Client/src/components/GameType.tsx
+++ Client/src/components/GameType.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function GameType({ title }: { title: string }) {  
     const [streak, setStreak] = useState(0);
